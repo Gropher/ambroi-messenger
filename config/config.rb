7b50561b6f0cac96629d656fb11783dc05efda5f
gropher <grophen@gmail.com>
2017-09-07 18:15:30 +0300

    config fixed

--- config/config.rb
+++ config/config.rb
@@ -2,4 +2,4 @@ REDIS_HOST = ENV['REDIS_HOST'] || 'redis'
 REDIS_PORT = ENV['REDIS_PORT'] || 6379 
 REDIS_DB = ENV['REDIS_DB'] || 0
 REDIS_CONFIG = "redis://#{REDIS_HOST}:#{REDIS_PORT}/#{REDIS_DB}"
-PORT = (IO.read('/run/secrets/messenger_port') rescue 7000)
+PORT = (IO.read('/run/secrets/messenger_port') rescue 5000)
